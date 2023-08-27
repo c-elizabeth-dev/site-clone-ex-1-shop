7dffd2275f0d31f2d9e3d3079a902913c03dc657
c-elizabeth-dev <c.elizabeth.keane@gmail.com>
2023-08-27 23:38:43 +0100

    first

---
+++ script.js
@@ -0,0 +1,16 @@
+const header = document.getElementById('header-frame'); 
+const logo = document.querySelector('.headerlogo');
+
+let isScrolled = false; 
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0 && !isScrolled) {
+    header.classList.add('scrolled');
+    logo.src = '/Media/AM logo.png'; 
+    isScrolled = true;
+  } else if (window.scrollY === 0 && isScrolled) {
+    header.classList.remove('scrolled');
+    logo.src = '/Media/Astrid-and-Miyu-Logo white.png'; 
+    isScrolled = false;
+  }
+});
