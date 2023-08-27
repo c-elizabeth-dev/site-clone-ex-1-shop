50c865d85f96144fe2d553ee85eec32a7af4c8c8
c-elizabeth-dev <c.elizabeth.keane@gmail.com>
2023-08-28 00:17:53 +0100

    js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let isScrolled = false;
 window.addEventListener('scroll', () => {
   if (window.scrollY > 0 && !isScrolled) {
     header.classList.add('scrolled');
-    logo.src = '/Media/AM logo.png'; 
+    logo.src = 'Media/AM logo.png'; 
     isScrolled = true;
   } else if (window.scrollY === 0 && isScrolled) {
     header.classList.remove('scrolled');
-    logo.src = '/Media/Astrid-and-Miyu-Logo white.png'; 
+    logo.src = 'Media/Astrid-and-Miyu-Logo white.png'; 
     isScrolled = false;
   }
 });
